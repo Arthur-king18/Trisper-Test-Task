a23d41676b1b5210c7a8e213a39f1a7cbf4c3ae9
Arthur-king18 <arturik060406@gmail.com>
2024-02-07 22:40:34 +0400

    delete file

--- core/exceptions/credits.py
+++
@@ -1,6 +0,0 @@
-from core.exceptions import CustomException
-
-class CreditsNotEnoughException(CustomException):
-    code = 402
-    error_code = "CREDITS__ARE_NOT_ENOUGH"
-    message = "credits are not enough"
